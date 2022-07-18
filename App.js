89303af50787dc9f4bc91048759fe5c53b6a9814
codewithdarkwa <jnrdrkw1@gmail.com>
2022-07-18 11:50:05 +0000

    style the status bar

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigator from './navigation/DrawerNavigator';
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
+    <>
    <NavigationContainer>
     <DrawerNavigator />
    </NavigationContainer>
+   <StatusBar style="dark"/>
+    </>
   );
 }
 
